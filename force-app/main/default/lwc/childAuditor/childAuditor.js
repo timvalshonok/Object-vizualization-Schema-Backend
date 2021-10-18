5fcb890955e4da5f4f149307ffde07aa66aa03a3
tsimaphei <tima.valshonok@gmail.com>
2021-10-18 09:30:47 +0300

    added backend-lwc-childauditor

--- force-app/main/default/lwc/childAuditor/childAuditor.js
+++ force-app/main/default/lwc/childAuditor/childAuditor.js
@@ -1,70 +1,3 @@
-// import { LightningElement, wire, api } from 'lwc';
-// import { refreshApex } from '@salesforce/apex';
-// import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-// import getChildRelation from '@salesforce/apex/ChildObjectsController.getChildRelation';
-
-// export default class ChildAuditor extends LightningElement {
-
-//     objectName;
-//     recordId;
-//     childObjects;
-
-//     connectedCallback(){
-//         [this.objectName, this.recordId] = window.location.pathname.split('/').slice(3,5);
-//     }
-
-//     @wire(getObjectInfo, { objectApiName: '$objectName' })
-//     objectInfo(result) {
-//         if(result.data) {
-//             this.showChildObjects(result.data.childRelationships);
-//             // this.objectInfo = data;
-//             // console.log(data);
-//             // this.searchProperties = this.objectInfo?.childRelationships.map( row => {
-//             //     return ({childObjectApiName: row.childObjectApiName, fieldName: row.fieldName})
-//             // });
-//             // console.log(this.searchProperties);
-//         } else if(result.error) {
-//             console.log(error);
-//         }
-//     }
-
-//     showChildObjects(data) {
-//         let childRelationships = {};
-//         data.forEach((child) => {
-//             childRelationships[child.childObjectName] = child.fieldName;
-//         })
-//         getChildRelation({childRelationships: childRelationships, recordId: this.recordId})
-//         .then((result) => {
-//             let childObjects = [];
-//             Object.keys(result).forEach((key) => {
-//                 childObjects.push({key: key, value: result[key]});
-//             })
-//             this.childObjects = childObjects;
-//         })
-//         .catch((error) => {
-//             console.log(error);
-//         })
-//     }
-
-//     // refreshComponent(event){
-//     //     return refreshApex(this.childRecords);
-//     // }
-    
-//     // @wire(getAccountChildRecords,{accountId: '$accountId'})
-//     // wiredResult(value) {
-//     //     this.accountChildRecords = value;
-//     //     const {data,error} = value;
-//     //     if (data) {
-//     //         console.log(data);
-//     //         this.accountRecordsCount = Object.keys(data[0]).map(key => {
-//     //             return key !='Id' ? {name: key, length: data[0][key].length} : ''});
-//     //         console.log(this.childRecordsCount);
-//     //     } else if (error) {
-//     //         console.log(error);
-//     //     }
-//     // }
-// }
-
 import { LightningElement, wire } from 'lwc';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import { refreshApex } from '@salesforce/apex';
@@ -86,6 +19,7 @@ import getChildrelationships from '@salesforce/apex/ChildObjectsController.getCh
         this.objectResponse = result;
         if(result.data){
             this.showChildObjects(result.data.childRelationships);
+            console.log(result.data);
         }
         if(result.error){
             console.log(result.error);
@@ -97,6 +31,7 @@ import getChildrelationships from '@salesforce/apex/ChildObjectsController.getCh
         data.forEach((child) => {
             childRelationships[child.childObjectApiName] = child.fieldName;
         })
+        console.log(childRelationships);
         getChildrelationships({childRelationships: childRelationships, recordId: this.recordId})
         .then((result) => {
             let childObjects = [];
@@ -104,6 +39,7 @@ import getChildrelationships from '@salesforce/apex/ChildObjectsController.getCh
                 childObjects.push({field: key, value: result[key]});
             })
             this.childObjects = childObjects;
+            console.log(this.childObjects);
         })
         .catch((error) => {
             console.log(error);
